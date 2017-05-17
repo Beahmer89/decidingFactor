45eff660304d096dbb8dfc9f7c5145df0dea8b3c
David Beahm <davidb@aweber.com>
2017-05-16 20:50:11 -0400

    Login/Signup pages created w/urls to direct them to correct page

--- decidingFactor/restaurant/urls.py
+++ decidingFactor/restaurant/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^login/$', auth_views.login),
+    url(r'^signup/$', views.signup),
 ]
