019c080cfe3417ecc43f641b209c5ee76aa92be8
David Beahm <davidb@aweber.com>
2017-05-21 22:25:58 -0400

    Defaults added to model and registered models to admin

--- decidingFactor/restaurant/models.py
+++ decidingFactor/restaurant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Search(models.Model):
+    user = models.ForeignKey(User, default=1)
+    search_name = models.CharField(max_length=50, default=None)
+    zip_code = models.IntegerField(default=0)
+    city = models.CharField(max_length=50, blank=True)
+    search_term = models.CharField(max_length=30, default=None)
+
+    def __str__(self):
+        return self.search_name
+
+
+class Restaurant(models.Model):
+    search = models.ForeignKey(Search, default=1)
+    name = models.CharField(max_length=50, default=None)
+    last_dining = models.DateField(default='null')
+    times_gone = models.IntegerField(default=0)
+    loved = models.BooleanField(default='False')
+    liked = models.BooleanField(default='False')
+    hated = models.BooleanField(default='False')
+    trying = models.BooleanField('False')
+
+    def __str__(self):
+        return self.name
