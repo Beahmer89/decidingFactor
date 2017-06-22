5cd0a8d0b0c60b1a69756e5eef1540e1e05575a0
David Beahm <davidb@aweber.com>
2017-06-21 20:02:05 -0400

    DB Enhancements - Model rework, db migrations, and admin incorporates new models

--- decidingFactor/restaurant/admin.py
+++ decidingFactor/restaurant/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from models import Search, Restaurant
+from models import Location, SearchHistory, Restaurant, VisitHistory
 
 # Register your models here.
-admin.site.register(Search)
+admin.site.register(Location)
+admin.site.register(SearchHistory)
 admin.site.register(Restaurant)
+admin.site.register(VisitHistory)
--- decidingFactor/restaurant/models.py
+++ decidingFactor/restaurant/models.py
@@ -1,27 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import uuid
 
-class Search(models.Model):
-    user = models.ForeignKey(User, default=1)
-    search_name = models.CharField(max_length=50, default=None)
+
+class Location(models.Model):
+    location_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                                   editable=False)
     zip_code = models.IntegerField(default=0)
     city = models.CharField(max_length=50, blank=True)
-    search_term = models.CharField(max_length=30, default=None)
 
     def __str__(self):
-        return self.search_name
+        if self.zip_code:
+            return self.zip_code
+        return self.city
 
+
+class SearchHistory(models.Model):
+    search_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                                 editable=False)
+    location_id = models.ForeignKey(Location)
+    search_terms = models.CharField(max_length=30, null=True, default=None)
+
+    def __str__(self):
+        return self.search_terms
 
 class Restaurant(models.Model):
-    search = models.ForeignKey(Search, default=1)
-    name = models.CharField(max_length=50, default=None)
-    last_dining = models.DateField(default='null')
-    times_gone = models.IntegerField(default=0)
-    loved = models.BooleanField(default='False')
-    liked = models.BooleanField(default='False')
-    hated = models.BooleanField(default='False')
-    trying = models.BooleanField('False')
+    restaurant_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                                     editable=False)
+    name = models.CharField(max_length=50, null=True, default=None)
+    restaurant_type = models.CharField(max_length=50, null=True, default=None)
+    price = models.CharField(max_length=10, null=True, default=None)
 
     def __str__(self):
         return self.name
+
+class VisitHistory(models.Model):
+    RATING = (('undecided', 'Undecided'), ('hate','Hated'),
+              ('like','Liked'), ('love','Loved'))
+    user_id = models.ForeignKey(User)
+    search_id = models.ForeignKey(SearchHistory)
+    restaurant_id = models.ForeignKey(Restaurant)
+    rating = models.CharField(max_length=10, default='undecided', choices=RATING)
+    last_visted = models.DateField(default='null')
+
+    def __str__(self):
+        return self.rating
