f6bf385aa5a61b2ce7e24af02feec09ceddd5ce0
David Beahm <davidb@aweber.com>
2019-03-31 16:06:31 -0400

    Adding DB fixtures and reworking basic test.

--- decidingFactor/restaurant/tests/test_restaurant_view.py
+++ decidingFactor/restaurant/tests/test_restaurant_view.py
@@ -7,22 +7,17 @@ from restaurant.tests import helpers
 
 
 class RestaurantTests(TestCase):
+    fixtures = ['new.yaml']
+
     def setUp(self):
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(
-            username='Death', email='reaper@grim.com', password='Muerte666')
+        self.user = User.objects.get(username='test')
         self.terms = 'vegan'
-        self.portland = Location.objects.create(city='Portland')
-        self.search = SearchHistory.objects.create(user_id=self.user,
-                                                   location_id=self.portland,
-                                                   search_terms=self.terms)
-        helpers.new_create_visit_history(self.portland, self.terms,
-                                         self.search, self.user)
 
     def test_success(self):
         request = self.factory.post('/restaurant/',
                                     {'name': 'Death', 'terms': 'vegan',
-                                     'city': self.portland})
+                                     'city': 'portland'})
         request.user = self.user
         response = views.restaurant(request)
         self.assertEquals(response.status_code, 200)
